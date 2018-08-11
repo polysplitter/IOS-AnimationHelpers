abf1200b09205f28814a495648da6e996f1a3ff0
Kyle <polysplitter@gmail.com>
2018-08-11 11:31:56 -0500

    setup project.

---
+++ AnimationHelpers/CustomView.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class CustomView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        defaultInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        defaultInit()
+    }
+    
+    func defaultInit() {
+        self.backgroundColor = UIColor.blue
+    }
+}
